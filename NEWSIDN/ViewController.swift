9bf8fcc1bf4ec1420fce1335ea0339c83d4784ee
Hafizh Caesandro Kevin <hake.protest@gmail.com>
2023-02-23 18:58:28 +0700

    Project Init

---
+++ NEWSIDN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NEWSIDN
+//
+//  Created by Hafizh Caesandro Kevinoza on 23/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
